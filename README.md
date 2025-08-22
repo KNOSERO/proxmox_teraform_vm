6f4955fdca9556f674e963ad6c2b96a47e9c7c41
Knosero <rafalpilecki.szczecin@gmail.com>
2025-08-22 17:03:55 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# proxmox_teraform_vm
+# Proxmox Terraform VM
 Reusable Terraform configuration for creating VMs in Proxmox
+
+## Env
+You create a file named `env.tfvars`
+
+```dotenv
+pm_password        = "<<PROXMOX PASSWORD>>"
+ci_password        = "<<PASSWORD VM>>"
+ci_password_hashed = "<<PASSWORD VM HASHED>>"
+```
+
+## Config
+You create a file named `variables.tfvars`
+
+```hcl
+containers = [
+  {
+    name         = "<<NAME CONTAINER>>"
+    target_node  = "<<NAME NODE>>"
+    vmid         = 201
+    cores        = 2
+    memory       = 4096
+    balloon      = 2048
+    ip_address   = "192.168.0.3/24"
+    mac          = "<<ADRES MAC>>"
+  },
+  {
+    name         = "<<NAME CONTAINER>>"
+    target_node  = "<<NAME NODE>>"
+    vmid         = 202
+    cores        = 2
+    memory       = 4096
+    balloon      = 2048
+    ip_address   = "192.168.0.4/24"
+    mac          = "<<ADRES MAC>>"
+  }
+]
+```
+
+-----
+
+You should also override the root user with reduced privileges in the target application
---
+++ main.tf
@@ -0,0 +1,66 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "telmate/proxmox"
+      version = "3.0.1-rc6"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url      = var.pm_api_url
+  pm_user         = var.pm_user
+  pm_password     = var.pm_password
+  pm_tls_insecure = true
+}
+
+resource "proxmox_vm_qemu" "ubuntu_vm" {
+  for_each    = {for c in var.containers : c.name => c}
+  name        = each.value.name
+  target_node = each.value.target_node
+  vmid        = each.value.vmid
+  clone       = "template"
+  full_clone  = true
+
+  cores       = each.value.cores
+  memory      = each.value.memory
+  balloon     = each.value.balloon
+  sockets     = 1
+
+  bios        = "ovmf"
+  machine     = "virt"
+  boot        = "order=scsi0"
+  bootdisk    = "scsi0"
+  scsihw      = "virtio-scsi-pci"
+  agent       = 1
+
+  serial {
+    id   = 0
+    type = "socket"
+  }
+
+  disk {
+    type    = "disk"
+    storage = "host_vm"
+    size    = "32G"
+    slot    = "scsi0"
+  }
+
+  disk {
+    type    = "cloudinit"
+    storage = "core_data"
+    slot    = "scsi1"
+  }
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+    id     = 0
+    macaddr = each.value.mac
+  }
+
+  ciuser     = var.ci_user
+  cipassword = var.ci_password_hashed
+  sshkeys    = file(var.path_ssh_key)
+  ipconfig0  = "ip=${each.value.ip_address},gw=192.168.0.1"
+}
